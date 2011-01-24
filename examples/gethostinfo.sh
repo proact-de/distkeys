c9aaccea8ef6d8fd26334199fdb26d036e51fbb3
Martin Steigerwald <ms@teamix.de>
2011-01-24 15:45:54 +0100

    Extend the example gethostinfo.sh script a bit ;).

--- examples/gethostinfo.sh
+++ examples/gethostinfo.sh
@@ -1,5 +1,15 @@
 #!/bin/sh
 
-test -f /etc/debian_version && ( echo -n "Debian: "; cat /etc/debian_version )
-test -f /etc/SuSE-release   && ( echo -n "SuSE:   "; cat /etc/SuSE-release )
-echo -n "Kernel: " ; uname -a
+echo "===== HOST INFORMATION ====="
+echo -n "Hostname: " ; hostname
+
+test -f /etc/debian_version && ( echo -n "Debian:   "; cat /etc/debian_version )
+test -f /etc/SuSE-release   && ( echo -n "SuSE:     "; cat /etc/SuSE-release )
+test -f /etc/redhat-release && ( echo -n "RedHat:   "; cat /etc/redhat-release )
+
+echo -n "Kernel:   " ; uname -a
+
+if [ -f /etc/debian_version ]; then
+	echo -e "\nSome installed software:"
+	COLUMNS=80 dpkg -l | egrep "( apache2 | dansguardian | openvpn | postfix | squid|nagios)" | cut -c 1-30
+fi
