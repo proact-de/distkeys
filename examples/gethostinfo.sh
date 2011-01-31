6e9f9ff0f6b7e6b215e863e7dacad10dfb0c7259
Sebastian Harl <sh@tokkee.org>
2011-01-31 12:46:09 +0100

    examples/gethostinfo.sh: Be more verbose about installed software.
     * get information from 'apt-cache'
     * determine running processes

--- examples/gethostinfo.sh
+++ examples/gethostinfo.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#! /bin/bash
 
 echo "===== HOST INFORMATION ====="
 echo -n "Hostname: " ; hostname
@@ -8,8 +8,18 @@ test -f /etc/SuSE-release   && ( echo -n "SuSE:     "; cat /etc/SuSE-release )
 test -f /etc/redhat-release && ( echo -n "RedHat:   "; cat /etc/redhat-release )
 
 echo -n "Kernel:   " ; uname -a
+echo -n "Uptime:   " ; uptime
 
 if [ -f /etc/debian_version ]; then
 	echo -e "\nSome installed software:"
-	dpkg -l | egrep "( apache2 | dansguardian | openvpn | postfix | squid|nagios)" | cut -c 1-50
+	dpkg -l | egrep "( squid|nagios)" | cut -c 1-50
+	for software in apache2 dansguardian openvpn postfix squid squid3 nagios2 nagios3; do
+		proc=$software
+
+		if test $proc = postfix; then proc=master; fi
+
+		echo "Package information for $software:"
+		LANG=C apt-cache policy $software | grep -E '^ ( Installed| Candidate|\*\*\*)'
+		echo -n "Running processes: " ; pidof $proc
+	done
 fi
