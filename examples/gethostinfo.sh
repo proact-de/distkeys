f857276f509c72cf85b2f40dcf1e4e6443178e56
Sebastian Harl <sh@tokkee.org>
2011-02-25 11:21:09 +0100

    examples/gethostinfo.sh: Query information from running Linux-VServer guests.

--- examples/gethostinfo.sh
+++ examples/gethostinfo.sh
@@ -20,7 +20,13 @@ test -f /etc/redhat-release \
 echo -n "Kernel:   " ; uname -a
 echo -n "Uptime:   " ; uptime
 
-if [ -f /etc/debian_version ]; then
+function get_debian_info() {
+	chroot_cmd=""
+
+	if [ -n "$1" ]; then
+		chroot_cmd="$@"
+	fi
+
 	echo -e "\nSome installed software:"
 	for software in apache2 dansguardian openvpn postfix squid squid3 \
 			nagios2 nagios3 mysql-server mysql-server-4.1 mysql-server-5.0 \
@@ -36,7 +42,7 @@ if [ -f /etc/debian_version ]; then
 				;;
 		esac
 
-		policy="$( apt-cache policy $software 2> /dev/null )"
+		policy="$( $chroot_cmd apt-cache policy $software 2> /dev/null )"
 		installed="$( echo "$policy" \
 			| grep -E '^  Installed:' | awk '{ print $2 }' )"
 		candidate="$( echo "$policy" \
@@ -53,7 +59,7 @@ if [ -f /etc/debian_version ]; then
 			fi
 
 			for proc in $procs; do
-				running="$( pidof $proc )"
+				running="$( $chroot_cmd pidof $proc )"
 				if test -n "$running"; then
 					echo "    Running '$proc' processes: $running"
 				else
@@ -64,16 +70,17 @@ if [ -f /etc/debian_version ]; then
 			case "$software" in
 				mysql-server*)
 					repl_conf_opts='bind.address|server.id|log.bin|binlog|report.host|relay.log'
-					repl_conf="$( grep -E '^[[:space:]]*('"$repl_conf_opts"')' \
+					repl_conf="$( $chroot_cmd grep -E '^[[:space:]]*('"$repl_conf_opts"')' \
 						/etc/mysql/my.cnf )"
 					if [ -n "$repl_conf" ]; then
 						echo "    MySQL Replication Setup:"
 						echo "$repl_conf" | while read line; do
 							echo "     > $line"
 						done
-						if test -n "$( pidof mysqld )"; then
+						if test -n "$( $chroot_cmd pidof mysqld )"; then
 							echo "    MySQL Replication Status:"
-							mysql --defaults-file=/etc/mysql/debian.cnf \
+							$chroot_cmd mysql \
+									--defaults-file=/etc/mysql/debian.cnf -t \
 									-e 'SHOW MASTER STATUS;' \
 									-e 'SHOW SLAVE STATUS;' \
 							| while read line; do
@@ -83,6 +90,24 @@ if [ -f /etc/debian_version ]; then
 					fi
 					;;
 			esac
+		fi
+	done
+}
+
+if [ -f /etc/debian_version ]; then
+	get_debian_info
+fi
+
+vserver_stat=$( which vserver-stat || echo /usr/sbin/vserver-stat )
+vserver=$( which vserver || echo /usr/sbin/vserver )
+if [ -x $vserver_stat -a -x $vserver ]; then
+	for vs in $( $vserver_stat | grep -E '^[[:digit:]]' | grep -Ev '^0\>' \
+			| awk '{ print $NF }' ); do
+		if $vserver $vs running > /dev/null 2>&1; then
+			if $vserver $vs exec test -f /etc/debian_version; then
+				echo "Linux-VServer guest '$vs':"
+				get_debian_info vserver $vs exec
+			fi
 		fi
 	done
 fi
